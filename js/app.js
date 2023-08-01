487adc9c7a2e860684a7e85c535318b95d01e712
tharindu <mustythemist@gmail.com>
2023-08-01 16:43:39 +0530

    about sec added

--- js/app.js
+++ js/app.js
@@ -3,3 +3,14 @@ gsap.from(".banner_desc", {
   y: 100,
   duration: 1
 });
+
+let scBtn = document.querySelector('.sc-top')
+
+document.addEventListener('scroll', () => {
+  console.log(window.scrollY);
+  if (window.scrollY > 1500) {
+    scBtn.classList.add('d-flex')
+  } else {
+    scBtn.classList.remove('d-flex')
+  }
+});
